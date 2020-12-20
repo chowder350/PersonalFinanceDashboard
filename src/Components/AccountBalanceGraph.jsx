e409ec74406945bbbf9885fbfe25935622bf5b66
Carlos Hernandez <chdez350@gmail.com>
2020-12-20 12:39:34 -0600

    stuff

---
+++ src/Components/AccountBalanceGraph.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class AccountBalanceGraph extends Component {
+  constructor() {
+    super()
+    this.state = {
+     
+    }
+  }
+
+componentDidMount = () => {
+    console.log('yo')
+}
+
+  render() {
+    return (
+      <div>
+     Yoohoo
+      </div>
+    )
+  }
+}
+
+export default AccountBalanceGraph;
