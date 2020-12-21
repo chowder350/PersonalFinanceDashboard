90e7e05e3e66dd2d3848d40503216b499106f608
Carlos Hernandez <chdez350@gmail.com>
2020-12-21 15:26:21 -0600

    got access token!

--- server/index.js
+++ server/index.js
@@ -25,18 +25,25 @@ app.get('/express_backend', (req, response) => {
   response.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
 
-//endpoint exchanges link token for an access token
-app.post('/exchange_public_token', async (request, response, next) => {
+//endpoint exchanges public token for an access token currently working on this part
+app.post('/exchange_public_token', async (request, res, next) => {
   try{
-    console.log(request.body)
-    console.log(public_token)
+  //public token cant be found
     const response = await client
-    .exchangePublicToken(public_token)
+    .exchangePublicToken(request.body.public_token)
     .catch((err) => {
       console.log(err)
     });
   const accessToken = response.access_token;
   const itemId = response.item_id;
+  // ACCESS_TOKEN = response.access_token;
+  // ITEM_ID = response.item_id;
+  res.json({
+    access_token: accessToken,
+    item_id: itemId
+  });
+  console.log("access token below");
+  console.log(accessToken);
   }
   catch(e){
     console.log(e)
